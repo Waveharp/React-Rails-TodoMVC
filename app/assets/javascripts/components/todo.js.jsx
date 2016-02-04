64bd3d5c661fae1c3eb843ed8ce8b5d70876f351
Josh Stricklin <jostricklin@gmail.com>
2016-02-03 22:17:04 -0600

    working on todo list items

---
+++ app/assets/javascripts/components/todo.js.jsx
@@ -0,0 +1,43 @@
+var TodoItem = React.createClass({
+  render: function() {
+    return (
+      <li>
+        {this.props.todos}
+      </li>
+    );
+  }
+});
+
+var TodoInput = React.createClass({
+  render: function() {
+    return (
+      <form>
+        <input type="text" placeholder="What needs to be done?" />
+      </form>
+    );
+  }
+});
+
+var TodoList = React.createClass({
+  render: function() {
+    return (
+      <div>
+        <TodoInput />
+        <ul>
+          <TodoItem />
+        </ul>
+      </div>
+    );
+  }
+});
+
+var TODOS = [
+  {name: 'Finish todo list'},
+  {name: "Don't mess this up"},
+  {name: 'Configure Vagrant box'}
+];
+
+// ReactDOM.render(
+//   <TodoList todos={TODOS} />,
+//   document.getElementById('container')
+// );
